3ded1cce9745704381096d7f04198618c4e9b18b
Liam Keegan <liam@keegan.ch>
2024-06-04 10:37:25 +0200

    Use Argon2 as default password hasher

--- django_test/settings.py
+++ django_test/settings.py
@@ -98,6 +98,14 @@ DATABASES = {
 }
 
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
